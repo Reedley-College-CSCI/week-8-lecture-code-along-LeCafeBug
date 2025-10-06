8bbec459939f31738e628b94267c37119dda3551
LeCafeBug <edgarjarquin3@gmail.com>
2025-10-06 14:44:29 -0700

    Added data and display for total, average, and longest yards.

--- game_stats.cpp
+++ game_stats.cpp
@@ -8,20 +8,44 @@ void showDrive()
 int main() {
     cout << "Detroit Lions at Baltimore Ravens" << endl
     << "Sept 29, 2025 Game Statistics" << endl;
-    
+    int size = 10;
     int yards[10] = {67, 98, 60, 94, 35, 25, 45, 50, 30, 40};
     cout << "First drive yards: " << yards[0] << endl;
     cout << "Epic second drive: " << yards[1] << " yards" << endl;
 
-    for (int 1 = 0; i < SIZE) {
+    for (int 1 = 0; i < size) {
         cout << "Drive " << i + 1 << ": " << yards[i] << " yards" << endl;
-
     }
 
     showDrive();
     showDrive();
     //display memory of array
     cout << "Array memory: ~" << sizeof(yards) << " bytes" << endl;
+    cout << "All Yards: " << endl;
+    for (int val : yards) {
+        cout << val << " ";
+    }
+
+    double total = 0;
+    for (int val : yards) {
+        total += val;
+    }
+    cout << "Total yards: " << total << endl;
+    cout << "Average yards: " << total / size << endl;
+
+    int maxYards = yards[0];
+    for (int i = 1; i < size; i++) {
+        if (yards[i] > maxYards)
+            maxYards = yards[i];
+    }
+    cout << "Longest drive: " << maxYards << " yards" << endl;
+
+    int plays[size] = (11, 18, 10, 12, 5, 4, 6, 7, 5, 6);
+    for (int i = 0; i < size; i++) {
+        cout << "Drive " << i + 1 << ": " << plays[i] << endl;
+
+    }
+
 
     return 0;
 }
