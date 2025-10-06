99af7dd5be4655ad4d10478070ee723b23d88449
LeCafeBug <edgarjarquin3@gmail.com>
2025-10-06 14:07:39 -0700

    Added arrey decleration for yards

--- game_stats.cpp
+++ game_stats.cpp
@@ -5,5 +5,11 @@ using namespace std;
 int main() {
     cout << "Detroit Lions at Baltimore Ravens" << endl
     << "Sept 29, 2025 Game Statistics" << endl;
+    
+    int yards[10] = {67, 98, 60, 94, 35, 25, 45, 50, 30, 40};
+    cout << "First drive yards: " << yards[0] << endl;
+    cout << "Epic second drive: " << yards[1] << " yards" << endl;
+
+    
     return 0;
 }
